a1fbb76afca5709be3abca7359f21c89054453ae
Mripoll-0 <gtmripoll@gmail.com>
2022-07-20 16:27:38 -0300

    Agregado nuevo archivo, archivo-ejemplo.js

---
+++ archivo-ejemplo.js
@@ -0,0 +1,22 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. Todos los derechos reservados.
+
+Instale la versión más reciente de PowerShell para obtener nuevas características y mejoras. https://aka.ms/PSWindows
+
+PS C:\Users\gtmri\Desktop> mkdir CarpetaHenry
+
+
+    Directorio: C:\Users\gtmri\Desktop
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----         20/7/2022     15:49                CarpetaHenry
+
+
+PS C:\Users\gtmri\Desktop> cd CarpetaHenry
+PS C:\Users\gtmri\Desktop\CarpetaHenry> git clone https://github.com/Mripoll-0/repositorio-henry.git
+Cloning into 'repositorio-henry'...
+warning: You appear to have cloned an empty repository.
+PS C:\Users\gtmri\Desktop\CarpetaHenry> cd repositorio-henry
+PS C:\Users\gtmri\Desktop\CarpetaHenry\repositorio-henry> touch archivo-ejemplo.js
